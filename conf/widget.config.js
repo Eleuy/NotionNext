8834ea0ff5c9ff6c6e962f6a65cc16015b54329f
Eleuy <eleuy172@gmail.com>
2025-06-22 16:28:57 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -18,7 +18,7 @@ module.exports = {
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
   WIDGET_PET_LINK:
     process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
-    'https://github.com/imuncle/live2d/blob/master/live2d_3/model/Azue%20Lane(JP)/xianghe_2/xianghe_2.model3.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
+    'https://raw.githubusercontent.com/imuncle/live2d/refs/heads/master/live2d_3/model/Azue%20Lane(JP)/chuixue_3/chuixue_3.model3.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
   WIDGET_PET_SWITCH_THEME:
     process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || false, // 点击宠物挂件切换博客主题
 
